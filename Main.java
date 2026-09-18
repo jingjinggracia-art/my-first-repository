0ea05e206de844d0014c3b0e1458329a4f643f0e
JeoMil Gracia <jingjinggracia@gmail.com>
2026-09-17 17:48:52 -0700

    Add files via upload
    Add Movie catalog machine problem

---
+++ Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+   public static void main(String[] args) {
+   
+      Movie m1 = new Movie();
+      m1.title = "Avengers";
+      m1.genre = "Action, SciFi, Fantasy";
+      m1.duration = 120;
+      m1.displayInfo();
+      
+      Movie m2 = new Movie();
+      m2.title = "Hercules";
+      m2.genre = "Action";
+      m2.duration = 95 ;
+      m2.displayInfo();
+      
+      Movie m3 = new Movie();
+      m3.title = "Cars";
+      m3.genre = "Comedy, Racing";
+      m3.duration = 90;
+      m3.displayInfo();
+      
+      
+      }
+    }
+      
+      
+      
+
+   
+   
+   
+   
---
+++ Movie.java
@@ -0,0 +1,10 @@
+public class Movie {
+
+   String title;
+   String genre;
+   int duration;
+   
+      void displayInfo() {
+      System.out.println("Movie title: " + title + " // " + "genre: " + genre + " // " + "duration: " + duration + " minutes");
+    }
+  }
